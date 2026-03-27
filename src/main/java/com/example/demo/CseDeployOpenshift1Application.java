46a44592c9c2fe4e25b61c0a66b18b62ba8a1a93
Lavanya Sundarapu <lavanyadevika1122@gmail.com>
2026-03-27 22:27:08 +0530

    first push

---
+++ src/main/java/com/example/demo/CseDeployOpenshift1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CseDeployOpenshift1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CseDeployOpenshift1Application.class, args);
+	}
+
+}
